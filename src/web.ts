c96b033f3ae123b1dc99abf4fc5a2df31ef42a91
bibyzan <benrasmonkey@gmail.com>
2019-08-02 10:54:03 -0400

    implemented web navigator watch position

--- src/web.ts
+++ src/web.ts
@@ -1,25 +1,53 @@
-import { WebPlugin } from '@capacitor/core';
-import { CapBackgroundGeoPlugin } from './definitions';
+import {WebPlugin} from '@capacitor/core';
+import {CapBackgroundGeoPlugin} from './definitions';
 
 export class CapBackgroundGeoWeb extends WebPlugin implements CapBackgroundGeoPlugin {
-  constructor() {
-    super({
-      name: 'CapBackgroundGeo',
-      platforms: ['web']
-    });
-  }
+    watchId: number = null;
 
-  async echo(options: { value: string }): Promise<{value: string}> {
-    console.log('ECHO', options);
-    return options;
-  }
+    constructor() {
+        super({
+            name: 'CapBackgroundGeo',
+            platforms: ['web']
+        });
+    }
 
-  async start(): Promise<any> {
-    return Promise.reject('Web not supported');
-  }
-  async stop(): Promise<any> {
-    return Promise.reject('Web not supported');
-  }
+    async echo(options: { value: string }): Promise<{ value: string }> {
+        console.log('ECHO', options);
+        return options;
+    }
+
+    async start(): Promise<void> {
+        if (this.watchId === null) {
+            if ('navigator' in window) {
+                this.watchId = navigator.geolocation.watchPosition((pos: Position) => {
+                        const {latitude, longitude} = pos.coords;
+                        const coords: any = {
+                            lat: latitude,
+                            lng: longitude
+                        };
+                        super.notifyListeners('geo-update', coords);
+                    },
+                    err => {
+                        console.error(err);
+                    },
+                    {
+                        enableHighAccuracy: true
+                    });
+                return Promise.resolve();
+            } else {
+                return Promise.reject('navigator not found in window');
+            }
+        } else {
+            return Promise.reject('Already watching location');
+        }
+    }
+
+    async stop(): Promise<void> {
+        if (this.watchId && 'navigator' in window) {
+            navigator.geolocation.clearWatch(this.watchId);
+            this.watchId = null;
+        }
+    }
 }
 
 const CapBackgroundGeo = new CapBackgroundGeoWeb();
